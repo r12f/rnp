e9c3eeaf59d7b485528151cb1ee499b1ac23277c
r12f <r12f.code@gmail.com>
2021-07-17 15:34:56 -0700

    Update tool description.

--- src/rnp_core_config.rs
+++ src/rnp_core_config.rs
@@ -3,7 +3,7 @@ use std::{time::Duration, path::PathBuf};
 
 pub const RNP_NAME: &str = "rnp";
 pub const RNP_AUTHOR: &str = "r12f (r12f.com, github.com/r12f)";
-pub const RNP_ABOUT: &str = "A simple cloud-friendly tool for testing network reachability.";
+pub const RNP_ABOUT: &str = "A simple layer 4 ping tool for cloud.";
 
 #[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
 pub enum RnpSupportedProtocol {
