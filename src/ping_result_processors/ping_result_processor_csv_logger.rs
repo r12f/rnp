8ccaac1fbd338a7dd7e68163f58c6b5cce45b05a
Riff <r12f.code@gmail.com>
2021-07-17 15:39:10 -0700

    Make serialized error in logs more organized and fix the csv log title. (#26)

--- src/ping_result_processors/ping_result_processor_csv_logger.rs
+++ src/ping_result_processors/ping_result_processor_csv_logger.rs
@@ -29,7 +29,7 @@ impl PingResultProcessor for PingResultProcessorCsvLogger {
     fn initialize(&mut self) {
         // Writer CSV header
         self.log_file
-            .write("UTCTime,WorkerId,Protocol,TargetIP,TargetPort,SourceIP,SourcePort,IsWarmup,RTTInMs,Error,IsPreparationError\n".as_bytes())
+            .write("UTCTime,WorkerId,Protocol,TargetIP,TargetPort,SourceIP,SourcePort,IsWarmup,RTTInMs,IsTimedOut,PreparationError,PingError\n".as_bytes())
             .expect(&format!(
                 "Failed to write logs to csv file! Path = {}",
                 self.log_path.display()
