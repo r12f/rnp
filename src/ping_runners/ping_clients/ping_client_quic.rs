8569f2d3fa26ea35d20305b8d1b604534c62c816
r12f <r12f.code@gmail.com>
2023-12-25 15:28:03 -0800

    Upgrade dependencies.

--- src/ping_runners/ping_clients/ping_client_quic.rs
+++ src/ping_runners/ping_clients/ping_client_quic.rs
@@ -1,7 +1,8 @@
 use crate::*;
 use async_trait::async_trait;
-use quinn::{ClientConfig, ClientConfigBuilder, ConnectionError, Endpoint, EndpointError};
-use rustls::ServerCertVerified;
+use quinn::{ClientConfig, ConnectionError, Endpoint, TransportConfig, IdleTimeout, EndpointConfig, TokioRuntime};
+use std::convert::TryFrom;
+use std::error::Error;
 use std::net::SocketAddr;
 use std::sync::Arc;
 use std::time::Instant;
@@ -17,7 +18,7 @@ impl PingClientQuic {
 
     #[tracing::instrument(name = "Running QUIC ping in ping client", level = "debug", skip(self))]
     async fn ping_target(&self, source: &SocketAddr, target: &SocketAddr) -> PingClientResult<PingClientPingResultDetails> {
-        let endpoint = self.create_local_endpoint(source).map_err(|e| PingClientError::PreparationFailed(Box::new(e)))?;
+        let endpoint = self.create_local_endpoint(source).map_err(|e| PingClientError::PreparationFailed(e))?;
         let server_name = self.config.server_name.as_ref().map_or("", |s| &s);
         let use_timer_rtt = self.config.use_timer_rtt;
         let ping_result = PingClientQuic::connect_to_target(&endpoint, source, target, server_name, use_timer_rtt).await;
@@ -25,50 +26,48 @@ impl PingClientQuic {
         return ping_result;
     }
 
-    fn create_local_endpoint(&self, source: &SocketAddr) -> Result<Endpoint, EndpointError> {
+    fn create_local_endpoint(&self, source: &SocketAddr) -> Result<Endpoint, Box<dyn Error + Send + Sync>> {
         let client_config = self.create_client_config();
 
-        let mut endpoint_builder = Endpoint::builder();
-        endpoint_builder.default_client_config(client_config);
+        let endpoint_config = EndpointConfig::default();
+        let runtime = Arc::new(TokioRuntime);
 
-        let socket = std::net::UdpSocket::bind(source).map_err(EndpointError::Socket)?;
+        let socket = std::net::UdpSocket::bind(source)?;
         if let Some(ttl) = self.config.time_to_live {
-            socket.set_ttl(ttl).map_err(EndpointError::Socket)?;
+            socket.set_ttl(ttl)?;
         }
 
-        let (endpoint, _) = endpoint_builder.with_socket(socket)?;
-        return Ok(endpoint);
+        let mut endpoint = Endpoint::new(endpoint_config, None, socket, runtime)?;
+        endpoint.set_default_client_config(client_config);
+
+        Ok(endpoint)
     }
 
     fn create_client_config(&self) -> ClientConfig {
-        let mut client_config_builder = ClientConfigBuilder::default();
+        let mut crypto = rustls::ClientConfig::builder()
+            .with_safe_defaults()
+            .with_custom_certificate_verifier(SkipServerVerification::new())
+            .with_no_client_auth();
 
         // Setup ALPN protocol if specified.
         if let Some(alpn_protocol) = &self.config.alpn_protocol {
             let protocols: &[&[u8]] = &[alpn_protocol.as_bytes()];
-            client_config_builder.protocols(protocols);
+            crypto.alpn_protocols = protocols.iter().map(|&x| x.into()).collect();
         }
-
+        
+        // TLS key log is used for debugging purpose.
         if self.config.log_tls_key {
-            client_config_builder.enable_keylog();
+            crypto.key_log = Arc::new(rustls::KeyLogFile::new());
         }
+    
+        let mut client_config = ClientConfig::new(Arc::new(crypto));
 
-        let mut client_config = client_config_builder.build();
-        {
-            let tls_cfg: &mut rustls::ClientConfig =
-                Arc::get_mut(&mut client_config.crypto).expect("Failed to get QUIC client crypto config, which should never happen.");
+        // Setting up transport config, i.e., Idle timeout.
+        let mut transport_config = TransportConfig::default();
+        transport_config.max_idle_timeout(Some(IdleTimeout::try_from(self.config.wait_timeout).expect("Convert from Duration to u64 failed. This should never happen.")));
+        client_config.transport_config(Arc::new(transport_config));
 
-            tls_cfg.dangerous().set_certificate_verifier(Arc::new(SkipCertificationVerification));
-
-            let transport_config =
-                Arc::get_mut(&mut client_config.transport).expect("Failed to get QUIC client transport config, which should never happen.");
-
-            transport_config
-                .max_idle_timeout(Some(self.config.wait_timeout))
-                .expect("Failed to set QUIC client max idle timeout, which should never happen.");
-        }
-
-        return client_config;
+        client_config
     }
 
     async fn connect_to_target(
@@ -80,7 +79,7 @@ impl PingClientQuic {
     ) -> PingClientResult<PingClientPingResultDetails> {
         let start_time = Instant::now();
 
-        let connecting = endpoint.connect(target, &server_name).map_err(|e| PingClientError::PingFailed(Box::new(e)))?;
+        let connecting = endpoint.connect(target.clone(), &server_name).map_err(|e| PingClientError::PingFailed(Box::new(e)))?;
         let connecting_result = connecting.await;
         let mut rtt = Instant::now().duration_since(start_time);
 
@@ -100,9 +99,9 @@ impl PingClientQuic {
             },
         }?;
 
-        let local_ip = connection.connection.local_ip().map_or(None, |addr| Some(SocketAddr::new(addr, source.port())));
+        let local_ip = connection.local_ip().map_or(None, |addr| Some(SocketAddr::new(addr, source.port())));
         if !use_timer_rtt {
-            rtt = connection.connection.rtt();
+            rtt = connection.rtt();
         }
         return Ok(PingClientPingResultDetails::new(local_ip, rtt, false, None));
     }
@@ -123,16 +122,24 @@ impl PingClient for PingClientQuic {
     }
 }
 
-struct SkipCertificationVerification;
+struct SkipServerVerification;
 
-impl rustls::ServerCertVerifier for SkipCertificationVerification {
+impl SkipServerVerification {
+    fn new() -> Arc<Self> {
+        Arc::new(Self)
+    }
+}
+
+impl rustls::client::ServerCertVerifier for SkipServerVerification {
     fn verify_server_cert(
         &self,
-        _roots: &rustls::RootCertStore,
-        _presented_certs: &[rustls::Certificate],
-        _dns_name: webpki::DNSNameRef,
+        _end_entity: &rustls::Certificate,
+        _intermediates: &[rustls::Certificate],
+        _server_name: &rustls::ServerName,
+        _scts: &mut dyn Iterator<Item = &[u8]>,
         _ocsp_response: &[u8],
-    ) -> Result<rustls::ServerCertVerified, rustls::TLSError> {
-        Ok(ServerCertVerified::assertion())
+        _now: std::time::SystemTime,
+    ) -> Result<rustls::client::ServerCertVerified, rustls::Error> {
+        Ok(rustls::client::ServerCertVerified::assertion())
     }
 }
